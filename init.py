4af4823c48de864221d5030d2eeaabdec155cc7c
Mineblocks107 <mineblocks107@gmail.com>
2025-06-13 20:12:58 +0530

    Add init command

---
+++ init.py
@@ -0,0 +1,7 @@
+import os
+
+def init():
+    os.mkdir("./__gitrem/branch/main/__stagingArea")
+    os.mkdir("./__gitrem/branch/main/__commits")
+    print("Initialized the repository.")
+    
--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#starters
+import argparse
+import init
+
+parser = argparse.ArgumentParser(description="A git remake done by mineblocks")
+
+parser = argparse.ArgumentParser(description="Command dispatcher")
+subparsers = parser.add_subparsers(dest='command', required=True)
+
+init_parser = subparsers.add_parser('init', help='Initialize the repository')
+
+add_parser = subparsers.add_parser('add', help='Add items to the staging area')
+add_parser.add_argument('-f', action='store_true', help='Enable f flag')
+add_parser.add_argument('value', help='Direct positional value')
+
+args = parser.parse_args()
+
+if args.command == 'init':
+    init.init()
+elif args.command == 'add':
+    pass
