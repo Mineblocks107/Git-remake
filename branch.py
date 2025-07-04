b0fc223cf8de2018f644722715276a8523a27b2b
Mineblocks107 <mineblocks107@gmail.com>
2025-07-04 16:04:28 +0530

    Added ability to add branch
    Also changes the way that repsitories are initialized to use the new add branch ability. Also relocated the current directory of repository to __gitremtest/.

--- branch.py
+++ branch.py
@@ -1,11 +1,11 @@
 import error
+import os
+import extrafunc
 
 def changeBranch(args):
     try:
         f = open("./__gitrem/__config.gitrem").read().splitlines()
-        print(f)
         f[1] = "branch = " + str(args)
-        print(f)
         fWrite = open("./__gitrem/__config.gitrem", "w")
         for i in f:
             fWrite.write(i + "\n")
@@ -18,6 +18,15 @@ def readBranch():
     except FileNotFoundError:
         error.RepoNotInitialized()
 
+def writeNewBranch(branch):
+    pastepath = ".\\__gitrem\\" + extrafunc.pad_path(branch, "__branch")
+    os.makedirs(pastepath + "\\__commits\\", exist_ok=True)
+    os.makedirs(pastepath + "\\__stagingArea\\", exist_ok=True)
+    changeBranch(branch)
+
+
 def branch(args):
     if args.c:
         changeBranch(args.c)
+    if args.a:
+        writeNewBranch(args.a)
--- init.py
+++ init.py
@@ -1,17 +1,14 @@
 import os
 import shutil
 import error
+import branch
 
 def __init(args):
     try:
         if args.reinit == True:
             rem()
         os.mkdir("./__gitrem/")
-        open("./__gitrem/__config.gitrem", "w").write("")
-        os.mkdir("./__gitrem/__branch/")
-        os.mkdir("./__gitrem/__branch/main/")
-        os.mkdir("./__gitrem/__branch/main/__commits")
-        os.mkdir("./__gitrem/__branch/main/__stagingArea")
+        branch.writeNewBranch("main")
         open("./__gitrem/__config.gitrem", "w").write("# __config.gitrem\nbranch = main")
         print("Initialized the repository.")
     except FileExistsError:
